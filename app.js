061bb941176832414ad168c1427c190e97be90d4
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 13:31:28 +0530

    sample

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-// const resume = require("./src/controllers/resumecontroller");
+const resume = require("./src/controllers/resumecontroller");
 
 // const passport = require("./src/configs/passport");
 
-// const productController = require("./src/controllers/product.controllers");
+const productController = require("./src/controllers/product.controllers");
 
-// const userController = require("./src/controllers/user.controller");
+const userController = require("./src/controllers/user.controller");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,12 +21,13 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.use("/",userController)
 
-// app.use("/internshala/resume", resume);
+app.use("/internshala/resume", resume);
 
 // app.use("/internshala", userController);
 
-// app.use("/internshala/products", productController);
+app.use("/internshala/products", productController);
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(
-    "mongodb+srv://riyazuddinshaik:58795879@cluster0.62qkq.mongodb.net/internshala"
-  );
+  return mongoose.connect("mongodb://127.0.0.1:27017/internshalaP");
 };
