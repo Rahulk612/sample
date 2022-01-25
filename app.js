7f87f42a8dd60c2a9acf0edba6b69f4f39fec541
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 15:23:52 +0530

    sample

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const resume = require("./src/controllers/resumecontroller");
 
-// // const passport = require("./src/configs/passport");
+const passport = require("./src/configs/passport");
 
 const productController = require("./src/controllers/product.controllers");
 
@@ -15,19 +15,55 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 
+app.use(passport.initialize());
+
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("index");
+  app.use("/",userController)
 });
 
-app.use("/",userController)
 
 app.use("/resume", resume);
 
-// app.use("/internshala", userController);
 
 app.use("/products", productController);
+
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/login/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+  }),
+  async (req, res) => {
+    let user = JSON.stringify(req.user.user);
+    return res.render("productPage", { user: user });
+  }
+);
+
+app.get("/auth/google/failure", (req, res) => {
+  return res.send("Failed");
+});
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
