2aa03dae06afdf6174014cdcfc22896fff1ba940
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 17:23:50 +0530

    sample

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+var methodOverride = require("method-override");
+
 const connect = require("./src/configs/db")
 
 const resume = require("./src/controllers/resumecontroller");
@@ -20,6 +22,21 @@ app.use(express.json());
 app.use(passport.initialize());
 
 app.use(express.static("public"));
+
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+
+
 
 app.get("/", function (req, res) {
   res.render("index");
