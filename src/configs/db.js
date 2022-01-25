bd2766e2e7e8675fb10c67b696251bb30e65296e
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 15:09:24 +0530

    sample

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,8 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 // module.exports = () => {
 //   return mongoose.connect(
-//     "mongodb+srv://rahul612:Rahul@123*@cluster0.4wgjt.mongodb.net/internshala"
+//     "mongodb+srv://riyazuddinshaik:58795879@cluster0.62qkq.mongodb.net/internshala"
 //   );
 // };
 const mongoose = require("mongoose");
@@ -10,7 +10,7 @@ require("dotenv").config();
 module.exports = () => {
   return mongoose.connect(
     process.env.MONGODB_URI ||
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4wgjt.mongodb.net/internshala`,
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.62qkq.mongodb.net/internshala`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
