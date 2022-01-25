9bacc107392f94f046acc519bfaef50ed3991231
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 12:52:19 +0530

    sample

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("index");
 });
-app.get("/new", function (req, res) {
-  res.send("New also worked");
-});
+
+
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
