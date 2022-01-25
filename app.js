b76cb116409f4d26c238034e3cf070430d0d8c2a
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 12:10:13 +0530

    sample

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+
 app.get("/", function (req, res) {
-  res.send("IT WORKED");
+  res.render("index");
 });
 app.get("/new", function (req, res) {
   res.send("New also worked");
